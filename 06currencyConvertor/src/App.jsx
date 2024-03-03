2bdb9e90b2f9a40caa1f7946744b68315cd09cd0
snehil-01 <visputesnehil2002@gmail.com>
2024-03-03 22:02:18 +0530

    6th fork from github

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,22 +1,62 @@
-import InputBox from "./components/InputBox";
-import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-    // const [cnt,setCnt] = useState(20);
-    // // useDemoHook(setCnt);
-    // // if(2){
-    // //     useDemoHook(21);
-    // // }
-    // useEffect(()=>{
-    //     console.log(`new cnt is ${cnt}`)
-    // },[cnt])
-    useCurrencyInfo('usd');
+  const [amt, setAmt] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  let cnt=0;
+  
+  let info;
 
-    return (<>
-        <h1 className="bg-green-400">Currency App</h1>
-        <InputBox/>
+  useEffect(() => {
+    // fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+    // .then((res) => res.json())
+    // .then(data => {
+    //   info=data[from];
+    // })
+    info =60
+  },[from])
+
+  return (
+    <>
+    <button onClick={() => cnt++}>+</button>
+    {cnt}
+    <button onClick={() => console.log(info)}>show</button>
+      <div>
+        <input type="number" />{" "}
+        <select
+          name="from"
+          id="from"
+          value={from}
+          onChange={(e) => setFrom(e.target.value)}
+        >
+          <option value="usd">USD</option>
+          <option value="inr">INR</option>
+          <option value="aus">AUS</option>
+        </select>
+      </div>
+
+      <button onClick={() => {
+        setFrom(to)
+        setTo(from)
+      }}>SWAP</button>
+
+      <div>
+        <input type="number" disabled />{" "}
+        <select
+          name="to"
+          id="to"
+          value={to}
+          onChange={(e) => setTo(e.target.value)}
+        >
+          <option value="usd">USD</option>
+          <option value="inr">INR</option>
+          <option value="aus">AUS</option>
+        </select>
+      </div>
     </>
-    )
+  );
 }
 
 export default App;
